694a77ea9c5511c62c02b12663b39527570df4c8
Vinayak <vinsyak318@gmail.com>
2025-06-10 08:57:19 +0530

    sum2number

---
+++ Sum2number.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Sum2number {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int a = sc.nextInt();
+        System.out.println("Enter second number:");
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
